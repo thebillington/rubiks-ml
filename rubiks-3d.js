9ec14a542063a016cec1301bfa101e35fb01b4c6
Billy Rebecchi <billy.rebecchi@googlemail.com>
2019-09-20 12:47:35 +0100

    Add colour to cube faces

--- rubiks-3d.js
+++ rubiks-3d.js
@@ -18,6 +18,7 @@ var rotationAxes;
 
 function setup() {
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xdddddd);
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
     renderer = new THREE.WebGLRenderer();
@@ -45,7 +46,7 @@ function initRubiksCube(x, y, z) {
     for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
             for (var k = 0; k < 3; k++) {
-                var thisCube = getBoxGeometery();
+                var thisCube = getBoxGeometery(i,j,k);
                 setCubePosition(thisCube, BASE_LOC + (i * CUBE_SIZE), BASE_LOC + (j * CUBE_SIZE), BASE_LOC + (k * CUBE_SIZE));
                 cubes.push(thisCube);
             }
@@ -111,7 +112,7 @@ function rotateSide() {
     rotationAmount++;
     if (rotationAmount == 90) {
         rotating = false;
-        resetRotation();
+        //resetRotation();
     }
 }
 
@@ -163,15 +164,58 @@ function rotateCubeAboutPoint(obj, theta, pointIsWorld) {
     rotateAboutPoint(obj.line, rotationPoint, rotationAxes, THREE.Math.degToRad(theta), pointIsWorld);
 }
 
-function getBoxGeometery() {
+var RIGHT_FACE = [0,1];
+var TOP_FACE = [4,5];
+var FRONT_FACE = [8,9];
+var LEFT_FACE = [2,3];
+var BOTTOM_FACE = [6,7];
+var BACK_FACE = [10,11];
+
+function getBoxGeometery(x, y, z) {
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    setFaceColours(geometry, x, y, z);
+    var material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
     var cube = new THREE.Mesh( geometry, material );
     var edges = new THREE.EdgesGeometry( geometry );
-    var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x000000 } ) );
+    var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 5 } ) );
     scene.add(cube);
     scene.add(line);
     return {cube: cube, line: line}
+}
+
+function setFaceColours(geometry, x, y, z) {
+    for (var i = 0; i < 2; i++) {
+        if (x == 2) {
+            geometry.faces[RIGHT_FACE[i]].color.setHex( 0xffa500 );
+        } else {
+            geometry.faces[RIGHT_FACE[i]].color.setHex( 0x000000 );
+        }
+        if (y == 2) {
+            geometry.faces[TOP_FACE[i]].color.setHex( 0x008000 );
+        } else {
+            geometry.faces[TOP_FACE[i]].color.setHex( 0x000000 );
+        }
+        if (z == 2) {
+            geometry.faces[FRONT_FACE[i]].color.setHex( 0xffffff );
+        } else {
+            geometry.faces[FRONT_FACE[i]].color.setHex( 0x000000 );
+        }
+        if (x == 0) {
+            geometry.faces[LEFT_FACE[i]].color.setHex( 0xff0000 );
+        } else {
+            geometry.faces[LEFT_FACE[i]].color.setHex( 0x000000 );
+        }
+        if (y == 0) {
+            geometry.faces[BOTTOM_FACE[i]].color.setHex( 0x0000ff );
+        } else {
+            geometry.faces[BOTTOM_FACE[i]].color.setHex( 0x000000 );
+        }
+        if (z == 0) {
+            geometry.faces[BACK_FACE[i]].color.setHex( 0xffff00 );
+        } else {
+            geometry.faces[BACK_FACE[i]].color.setHex( 0x000000 );
+        }
+    }
 }
 
 function rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
